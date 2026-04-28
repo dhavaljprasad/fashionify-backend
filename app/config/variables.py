c9ef714df7560affb96948c6d5d64ce3457b0841
dhavaljprasad <dhavaljprasad@gmail.com>
2026-04-28 10:57:23 +0530

    update: added few new packages and variables for auth and jwt

--- app/config/variables.py
+++ app/config/variables.py
@@ -2,5 +2,15 @@ import os
 
 
 class ConfigVariables:
+    # Database variables
     MONGO_URI: str = os.getenv("MONGO_URI")
     DATABASE_NAME: str = os.getenv("DATABASE_NAME")
+
+    # Google Auth variables
+    GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
+    GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET")
+
+    # JWT Auth variables
+    JWT_SECRET: str = os.getenv("JWT_SECRET")
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: str = os.getenv("JWT_ACCESS_TOKEN_EXPIRE_MINUTES")
+    JWT_ALGORITHM: str = os.getenv("JWT_ALGORITHM")

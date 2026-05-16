3edc74498aa109d110a8e475dfdafa56473cbf8f
dhavaljprasad <dhavaljprasad@gmail.com>
2026-05-17 04:50:46 +0530

    updated: added a new conversation query

--- app/database/queries/conversations.py
+++ app/database/queries/conversations.py
@@ -1,4 +1,6 @@
 from app.database.init import Conversations
+from app.database.models.conversations import EntryType
+from beanie import PydanticObjectId
 
 
 async def init_new_conversation_document(user_id: str):
@@ -8,4 +10,21 @@ async def init_new_conversation_document(user_id: str):
         return new_conversation_doc
     except Exception as e:
         print("Unexpected error occured init new conversation document as:", e)
+        return None
+
+
+async def update_conversation_type(conversation_id: str, conversation_type: EntryType):
+    try:
+        conversation_doc = await Conversations.find_one(
+            Conversations.conversation_id == PydanticObjectId(conversation_id)
+        )
+
+        conversation_doc.entry_type = conversation_type
+
+        await conversation_doc.save()
+
+        return conversation_doc
+
+    except Exception as e:
+        print("Unexpected error occured updating the conversation document as:", e)
         return None

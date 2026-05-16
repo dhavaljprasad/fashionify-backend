45321eb8b7b594e6635d593c1bf9b87216608415
dhavaljprasad <dhavaljprasad@gmail.com>
2026-05-17 04:51:15 +0530

    feat: added new imgkit functions

--- app/utils/imgkit.py
+++ app/utils/imgkit.py
@@ -1,6 +1,8 @@
 from imagekitio import ImageKit
 from app.config.variables import ConfigVariables
 
+import base64
+
 imagekit = ImageKit(
     private_key=ConfigVariables.IMGKIT_PRIVATE_KEY,
 )
@@ -26,4 +28,30 @@ def get_user_uploaded_images(user_id: str, conversation_id: str, file_name: str)
 
     except Exception as e:
         print("Unexpected error occured getting image URL:", e)
+        return None
+
+
+def upload_generated_see_on_image(
+    user_id: str, conversation_id: str, file_name: str, b64_image: str
+):
+    try:
+        folder = f"/{user_id}/generated/{conversation_id}"
+        image_bytes = base64.b64decode(b64_image)
+
+        response = imagekit.files.upload(
+            file=image_bytes,
+            file_name=file_name,
+            folder=folder,
+            use_unique_file_name=True,
+            overwrite_file=True,
+        )
+
+        return {
+            "url": response.url,
+            "file_id": response.file_id,
+            "file_path": f"{folder}/{file_name}",
+        }
+
+    except Exception as e:
+        print("Unexpected error occured saving generated see on image on imgkit as:", e)
         return None

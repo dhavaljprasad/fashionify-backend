cd09666f20ff0cae09b5b7b9c231ce2247252f43
dhavaljprasad <dhavaljprasad@gmail.com>
2026-05-17 04:49:07 +0530

    feat: added new pooling DB model

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -4,5 +4,6 @@ from app.database.models.conversations import Conversations
 from app.database.models.messages import Messages
 from app.database.models.images import Images
 from app.database.models.usage_events import Usage_Events
+from app.database.models.pooling import Pooling
 
-__all__ = [Users, Subscriptions, Conversations, Messages, Images, Usage_Events]
+__all__ = [Users, Subscriptions, Conversations, Messages, Images, Usage_Events, Pooling]
---
+++ app/database/models/pooling.py
@@ -0,0 +1,29 @@
+from beanie import Document, PydanticObjectId
+from pydantic import Field
+from enum import StrEnum
+from typing import Optional
+
+
+class Status(StrEnum):
+    PENDING = "pending"
+    COMPLETED = "completed"
+    FAILED = "failed"
+    NOT_STARTED = "not_started"
+
+
+class Generation(StrEnum):
+    SEE_ON = "see_on"
+    DRESS_UP = "dress_up"
+    THREE_SIXTY = "three_sixty"
+    ITERATION = "iteration"
+
+
+class Pooling(Document):
+    pooling_id: PydanticObjectId = Field(default_factory=PydanticObjectId, index=True)
+    user_id: PydanticObjectId
+    status: Status = Status.NOT_STARTED
+    pooling_type: Generation = Generation.SEE_ON
+    data: Optional[dict] = {}
+
+    class Settings:
+        name = "pooling"

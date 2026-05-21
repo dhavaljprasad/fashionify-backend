35a50d503fdb73fcc5eab20e24ea5c070390ecd3
dhavaljprasad <dhavaljprasad@gmail.com>
2026-05-22 05:09:42 +0530

    feat: added pooling functions for db operations

--- app/database/queries/pooling.py
+++ app/database/queries/pooling.py
@@ -1,4 +1,5 @@
 from app.database.init import Pooling
+from beanie import PydanticObjectId
 
 
 async def init_pooling_doc(user_id: str, pooling_type: str):
@@ -8,4 +9,33 @@ async def init_pooling_doc(user_id: str, pooling_type: str):
         return new_pooling_doc
     except Exception as e:
         print("Unexpected error occured in init_pooling mongo function as:", e)
+        return None
+
+
+async def update_pooling_status(pooling_id: str, status: str, data: dict = {}):
+    try:
+        pooling_doc = await Pooling.find_one(
+            Pooling.pooling_id == PydanticObjectId(pooling_id)
+        )
+        if pooling_doc is None:
+            print("Pooling document not found for pooling_id:", pooling_id)
+            return None
+
+        pooling_doc.status = status
+        pooling_doc.data = data
+        await pooling_doc.save()
+        return pooling_doc
+    except Exception as e:
+        print("Unexpected error occured in update_pooling_status mongo function as:", e)
+        return None
+
+
+async def get_pooling_info(pooling_id: str):
+    try:
+        pooling_doc = await Pooling.find_one(
+            Pooling.pooling_id == PydanticObjectId(pooling_id)
+        )
+        return pooling_doc
+    except Exception as e:
+        print("Unexpected error occured in get_pooling_info mongo function as:", e)
         return None

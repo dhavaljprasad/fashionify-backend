85000b5984dba282a1c08408a8e92d04de685249
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-11 03:57:44 +0530

    update: updated conversation schema to hold value for type of conversation

--- app/database/models/conversations.py
+++ app/database/models/conversations.py
@@ -10,10 +10,16 @@ class EntryType(StrEnum):
     CLOTHPIECE = "clothpiece"
 
 
+class ConversationType(StrEnum):
+    VISUALIZATION = "visualization"
+    STYLIST = "stylist"
+
+
 class Conversations(Document):
     user_id: PydanticObjectId
     conversation_id: PydanticObjectId = Field(default_factory=PydanticObjectId)
     entry_type: EntryType = EntryType.LINK
+    conversation_type: ConversationType = ConversationType.VISUALIZATION
     title: str = "New Conversation"
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))

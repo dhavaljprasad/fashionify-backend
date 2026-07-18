5d3fad52251d035fcaaafe83cdcf9fd43c1e5d4c
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-18 22:25:45 +0530

    fix shifted imports after loading envs to prevent from undefined variables

--- app/workers/celery.py
+++ app/workers/celery.py
@@ -1,10 +1,19 @@
 from celery import Celery
+from dotenv import load_dotenv
+import os
+
+loaded = load_dotenv(dotenv_path=os.path.join(os.getcwd(), ".env"))
+
 from app.config.variables import ConfigVariables
 
 celery_app = Celery(
-    "worker", broker=ConfigVariables.REDIS_URL, backend=ConfigVariables.REDIS_URL
+    "worker",
+    broker=ConfigVariables.REDIS_URL,
+    backend=ConfigVariables.REDIS_URL,
 )
 
 celery_app.autodiscover_tasks(["app.workers.tasks"])
-
-celery_app.conf.update(task_track_started=True, timezone="Asia/Kolkata")
+celery_app.conf.update(
+    task_track_started=True,
+    timezone="Asia/Kolkata",
+)

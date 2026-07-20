bacc4c605297c70ead516b76ce38a60332ead3e6
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-20 09:26:59 +0530

    feat: added support for more openai functions

--- app/ai/openai.py
+++ app/ai/openai.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import requests
+import json
 
 client = OpenAI()
 
@@ -43,4 +44,85 @@ def generate_image(
 
     except Exception as e:
         print("Unexpected error occured calling generate image function as", e)
+        return None
+
+
+def tool_call(
+    tools: list, custom_prompt: str, context: str, model: str = "gpt-4.1-mini"
+):
+    try:
+        response = client.chat.completions.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": custom_prompt},
+                {"role": "user", "content": context},
+            ],
+            tools=tools,
+            tool_choice="required",
+        )
+
+        message = response.choices[0].message
+        tool_call = message.tool_calls[0]
+
+        return json.loads(tool_call.function.arguments)
+
+    except Exception as e:
+        print(f"Unexpected error occured in tools_call as {e}")
+        return None
+
+
+def llm_call_without_images(
+    custom_prompt: str,
+    context: str,
+    model: str = "gpt-4.1-mini",
+    temp: float = 0.7,
+):
+    try:
+        response = client.responses.create(
+            model=model,
+            input=[
+                {
+                    "role": "system",
+                    "content": custom_prompt,
+                },
+                {
+                    "role": "user",
+                    "content": context,
+                },
+            ],
+            temperature=temp,
+        )
+
+        return response.output_text
+
+    except Exception as e:
+        print(f"Unexpected error occurred in ai function llm_call_without_images: {e}")
+        return None
+
+
+def llm_call_with_images(
+    custom_prompt: str,
+    context: list,
+    model: str = "gpt-4.1-mini",
+    temp: float = 0.7,
+):
+    try:
+        response = client.responses.create(
+            model=model,
+            input=[
+                {
+                    "role": "system",
+                    "content": custom_prompt,
+                },
+                {
+                    "role": "user",
+                    "content": context,
+                },
+            ],
+            temperature=temp,
+        )
+
+        return response.output_text
+    except Exception as e:
+        print(f"Unexpected error occurred in ai function llm_call_with_images: {e}")
         return None

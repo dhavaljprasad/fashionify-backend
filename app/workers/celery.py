f9e784098b41494ae2b9e86888b1995c5c760193
dhavaljprasad <dhavaljprasad@gmail.com>
2026-07-24 04:46:42 +0530

    fix: removed the logic for redis ssl between prod and dev

--- app/workers/celery.py
+++ app/workers/celery.py
@@ -7,14 +7,6 @@ celery_app = Celery(
     backend=ConfigVariables.REDIS_URL,
 )
 
-if ConfigVariables.ENVIRONMENT == "production":
-    celery_app.conf.broker_use_ssl = {
-        "ssl_cert_reqs": "CERT_NONE",
-    }
-    celery_app.conf.redis_backend_use_ssl = {
-        "ssl_cert_reqs": "CERT_NONE",
-    }
-
 celery_app.conf.update(
     task_track_started=True,
     timezone="Asia/Kolkata",
